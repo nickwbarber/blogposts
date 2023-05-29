db1aaca60eeb7c1f5cb8f88a66d874af403a1786
nickwbarber <nickwbarber@gmail.com>
2023-05-29 15:37:30 +0800

    Refactor configuration-related references
    break out into new files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const config = require('./utils/config')
+
 const app = express()
 
 const blogSchema = new mongoose.Schema({
@@ -14,7 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -37,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
