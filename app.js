a224cb12acdae8f82c484c1d838347dc3455d236
nickwbarber <nickwbarber@gmail.com>
2023-05-29 15:58:06 +0800

    Refactor models and controllers into their own directories

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// Desc: Main application file
+
 require('dotenv').config()
+
 const cors = require('cors')
-const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -10,32 +15,6 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(blogRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// Desc: Main entry point for the application
+
 const config = require('./utils/config')
 const app = require('./app')
-
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
