84f6c9d879fd3e21f3839ac25b471dab4d431eaa
nickwbarber <nickwbarber@gmail.com>
2023-06-01 16:17:40 +0800

    Define blog list tests

--- tests/blog_list.test.js
+++ tests/blog_list.test.js
@@ -1,3 +1,4 @@
+const blog_list = require('./blog_list')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
@@ -6,3 +7,18 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+describe('total likes', () => {
+  const blogs = blog_list.blogs
+  
+  test('of empty list is zero', () =>{
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+  
+  test('when list has only one blog equals the likes of that', () => {
+    expect(listHelper.totalLikes(blogs.slice(0,1))).toBe(7)
+  })
+
+  test('of a bigger list is calculated correctly', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,12 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
 
