34f02306283c4db97496a99ffd29bf866860c4ce
nickwbarber <nickwbarber@gmail.com>
2023-06-09 13:35:15 +0800

    Refactor for readability

--- tests/requests/post_blog.int.test.js
+++ tests/requests/post_blog.int.test.js
@@ -8,54 +8,59 @@ const mongoose = require('mongoose')
 
 const api = supertest(app)
 
+///////////////////////////////////////////
+/* Make some data available to all tests */
+const newBlog = {
+  title: "React patterns",
+  author: "Michael Chan",
+  url: `https://reactpatterns.com/test/${Math.round(Math.random()*1000)}`,
+  likes: 0,
+}
 
-describe('POST /api/blogs', () => {
-  const newBlog = {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: `https://reactpatterns.com/test/${Math.round(Math.random()*1000)}`,
-    likes: 0,
-  }
+let blogsBefore, postResponse, blogsAfter
 
-  describe('POST sends successfully', () => {
-    let blogsBefore, postResponse, blogsAfter
-  
-    const getBlogWithoutId = blog => {
-      const blogWithoutId = { ...blog }
-      delete blogWithoutId.id
-      return blogWithoutId
-    }
+beforeAll(async () => {
+  blogsBefore = await api.get('/api/blogs')
 
-    beforeAll(async () => {
-      blogsBefore = await api.get('/api/blogs')
+  postResponse = await api
+    .post('/api/blogs')
+    .send(newBlog)
 
-      postResponse =  await api
-        .post('/api/blogs')
-        .send(newBlog)
-
-      blogsAfter = await api.get('/api/blogs')
-    })
-
-    test('returns successfully', async () => {
-      expect(postResponse.status).toBe(201)
-    })
-
-    test('returns in application-json format', async () => {
-      expect(postResponse.get('content-type')).toMatch(/application\/json/)
-    })
-
-    test('new blog list is one more than before', () => {
-      expect(blogsAfter.body.length).toBe(blogsBefore.body.length + 1)
-    })
-
-    test('new blog list contains the new blog', () => {
-      expect(blogsAfter.body.map(getBlogWithoutId))
-        .toContainEqual(getBlogWithoutId(newBlog))
-    })
-  })
+  blogsAfter = await api.get('/api/blogs')
 })
-
 
 afterAll(async () => {
   mongoose.connection.close()
 })
+///////////////////////////////////////////
+
+
+///////////
+/* tests */
+describe('POST /api/blogs', () => {
+
+  const getBlogWithoutId = blog => {
+    const blogWithoutId = { ...blog }
+    delete blogWithoutId.id
+    return blogWithoutId
+  }
+
+  test('returns successfully', async () => {
+    expect(postResponse.status).toBe(201)
+  })
+
+  test('returns in application-json format', async () => {
+    expect(postResponse.get('content-type')).toMatch(/application\/json/)
+  })
+
+  test('new blog list is one more than before', () => {
+    expect(blogsAfter.body.length).toBe(blogsBefore.body.length + 1)
+  })
+
+  test('new blog list contains the new blog', () => {
+    expect(blogsAfter.body.map(getBlogWithoutId))
+      .toContainEqual(getBlogWithoutId(newBlog))
+  })
+})
+///////////
+
