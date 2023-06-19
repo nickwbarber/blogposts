eb2911dabc85059935cd63e3ca33325c6f7ea974
nickwbarber <nickwbarber@gmail.com>
2023-06-20 02:17:11 +0800

    Fix formatting (semicolons, etc)

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 // Desc: Main application file
 
-require('dotenv').config()
+require('dotenv').config();
 
 // external imports
-const cors      = require('cors')
-const express   = require('express')
-const mongoose  = require('mongoose')
-require('express-async-errors')
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+require('express-async-errors');
 
 // internal imports
-const config      = require('./utils/config')
-const blogRouter  = require('./controllers/blogs')
-const logger      = require('./utils/logger')
-const middleware  = require('./utils/middleware')
+const config = require('./utils/config');
+const blogRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
-const app = express()
+const app = express();
 
 try {
-  mongoose.connect(config.MONGODB_URI)
+  mongoose.connect(config.MONGODB_URI);
 } catch (err) {
-  logger.error('Error connecting to MongoDB:', err.message)
+  logger.error('Error connecting to MongoDB:', err.message);
 }
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogRouter)
+app.use('/api/blogs', blogRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
+
