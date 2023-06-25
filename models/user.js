7a257cafa24c0d71b1f73c6d163a587adf35550c
nickwbarber <nickwbarber@gmail.com>
2023-06-25 14:26:29 +0800

    Define User schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+// Desc: User model for MongoDB
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+userSchema.set('toJSON', {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+    delete returnedObj.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
