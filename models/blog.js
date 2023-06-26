fbc257d6239c69696a5d3d72f9d130b4330b9fa1
nickwbarber <nickwbarber@gmail.com>
2023-06-26 11:19:32 +0800

    Use semicolons

--- models/blog.js
+++ models/blog.js
@@ -1,23 +1,24 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
 // Desc: Blog model for MongoDB
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
 blogSchema.set('toJSON', {
   transform: (doc, returnedObj) => {
-    returnedObj.id = returnedObj._id.toString()
-    delete returnedObj._id
-    delete returnedObj.__v
-  }
-})
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-module.exports = Blog
+module.exports = Blog;
+
