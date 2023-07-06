c281e9e42b5df86e9bbfb7bb66da2429bc0401b1
nickwbarber <nickwbarber@gmail.com>
2023-07-06 11:36:38 +0800

    Add dummy User creation helper

--- utils/test_helper.js
+++ utils/test_helper.js
@@ -9,13 +9,23 @@ const getDummyBlogWithoutUser = () => {
   };
 };
 
-createDummyBlogs = async (n) => {
+const createDummyBlogs = async (n) => {
   for (let i = 0; i < n; i++) {
     Blog.create(getDummyBlogWithoutUser());
   }
 };
 
+const getDummyUser = () => {
+  return {
+    username: `TestUser${Math.round(Math.random() * 1000)}`,
+    name: `TestName${Math.round(Math.random() * 1000)}`,
+    passwordHash: `TestHash${Math.round(Math.random() * 1000)}`,
+    notes: [],
+  };
+};
+
 module.exports = {
-  getDummyBlogWithoutUser,
   createDummyBlogs,
+  getDummyBlogWithoutUser,
+  getDummyUser,
 };
