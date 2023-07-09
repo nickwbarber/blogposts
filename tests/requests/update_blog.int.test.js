3598d4de5195a81807e4818eb0b2441cb517b4a9
nickwbarber <nickwbarber@gmail.com>
2023-07-09 20:20:57 +0800

    Refactor Blog update test

--- tests/requests/update_blog.int.test.js
+++ tests/requests/update_blog.int.test.js
@@ -1,79 +1,66 @@
-// update_blog.int.test.js
-//
-// Desc: Test updating blogs
-
-const supertest = require('supertest');
-const mongoose = require('mongoose');
-const app = require('../../app');
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../../app");
+const Blog = require("../../models/blog");
+const { setupTestDB } = require("../../utils/test_helper");
 
 const api = supertest(app);
 
-/* Make some data available to all tests */
-
-const TEST_NUM_OF_LIKES = Math.floor(Math.random()*10000);
-const TEST_TITLE = `title-${Math.floor(Math.random()*10000)}`;
-const TEST_AUTHOR = `author-${Math.floor(Math.random()*10000)}`;
-const TEST_URL = `url-${Math.floor(Math.random()*10000)}`;
-
-let blogToUpdate;
-let blogsAfter;
-let blogsBefore;
-let responseOnUpdate;
-let blogAfterUpdate;
-
-beforeAll(async () => {
-  blogsBefore = (await api.get('/api/blogs')).body;
-  blogToUpdate = blogsBefore[blogsBefore.length - 1];
-  responseOnUpdate = await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send({
-      title: TEST_TITLE,
-      author: TEST_AUTHOR,
-      likes: TEST_NUM_OF_LIKES,
-      url: TEST_URL,
-    });
-  blogsAfter = (await api.get('/api/blogs')).body;
-  blogAfterUpdate = blogsAfter[blogsAfter.length - 1];
-});
-
 afterAll(async () => {
+  Blog.deleteMany({});
   mongoose.connection.close();
 });
 
-/* pre-run validity check */
-describe('pre-run', () => {
-  test('blogs were fetched', () => {
-    expect(blogsBefore).toBeDefined();
+describe("Updating the author of a blog", () => {
+  const TEST_NUM_OF_LIKES = Math.floor(Math.random() * 10000);
+  const TEST_TITLE = `title-${Math.floor(Math.random() * 10000)}`;
+  const TEST_AUTHOR = `author-${Math.floor(Math.random() * 10000)}`;
+  const TEST_URL = `url-${Math.floor(Math.random() * 10000)}`;
+
+  let blogsBefore;
+  let blogToUpdate;
+  let responseOnUpdate;
+  let blogsAfter;
+  let blogAfterUpdate;
+
+  beforeAll(async () => {
+    await setupTestDB({ numOfBlogs: 3, numOfUsers: 0 });
+    blogsBefore = await Blog.find({});
+    blogToUpdate = await Blog.findOne({});
+    console.log(blogToUpdate);
+    responseOnUpdate = await api
+      .put(`/api/blogs/${blogToUpdate._id.toString()}`)
+      .send({
+        title: TEST_TITLE,
+        author: TEST_AUTHOR,
+        likes: TEST_NUM_OF_LIKES,
+        url: TEST_URL,
+      });
+    blogsAfter = await Blog.find({});
+    blogAfterUpdate = await Blog.findOne({ _id: blogToUpdate._id });
   });
 
-  test('the blog to update was found', () => {
-    expect(blogToUpdate).toBeDefined();
-  });
-});
-
-/* actual tests */
-describe('update a blog', () => {
-  test('returns with code 200', async () => {
+  test("when sucessful, returns with code 200", async () => {
     expect(responseOnUpdate.status).toBe(200);
   });
 
-  test('does not alter blog list length', () => {
+  test("does not change the length of blog list", () => {
     expect(blogsAfter.length).toBe(blogsBefore.length);
   });
 
-  test('blog after update contains the right author', () => {
+  test("blog after update contains the right author", () => {
     expect(blogAfterUpdate.author).toBe(TEST_AUTHOR);
   });
 
-  test('blog after update contains the right title', () => {
+  test("blog after update contains the right title", () => {
     expect(blogAfterUpdate.title).toBe(TEST_TITLE);
   });
 
-  test('blog after update contains the right url', () => {
+  test("blog after update contains the right url", () => {
     expect(blogAfterUpdate.url).toBe(TEST_URL);
   });
 
-  test('blog after update contains the right number of likes', () => {
+  test("blog after update contains the right number of likes", () => {
     expect(blogAfterUpdate.likes).toBe(TEST_NUM_OF_LIKES);
   });
 });
