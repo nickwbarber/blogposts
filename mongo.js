2443e949c1c77afe5331a6fd14a803986d82d090
nickwbarber <nickwbarber@gmail.com>
2023-07-19 14:34:49 +0800

    Rewrite database helper script

--- mongo.js
+++ mongo.js
@@ -1,28 +1,43 @@
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+require("dotenv").config();
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const User = require("./models/user");
 
 // connect to db
 // determine what mode we are in based on the number of arguments
-const GET_INFO = 2
-const ADD_RECORD = 4
-const MODE = process.argv.length
+const GET_MODE = "get";
+const MODE = process.argv[2];
 
-// do stuff
-switch (MODE) {
-  case GET_INFO: {
-    Blog.find({}).then(result => {
-      console.log('blogs:')
-      result.forEach(blog => {
-        console.log(`
-          id: ${blog._id.toString()}
-          title: ${title},
-          author: ${author},
-          url: ${url},
-          likes: ${likes},
-        `)
-      })
-      mongoose.connection.close()
-    })
-    break
+// connect to the database
+(async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URI);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
   }
-}
+})();
+
+const getInfo = async (target) => {
+  if (target === "blogs" || target === "all") {
+    const blogs = await Blog.find({});
+    console.log("blogs:", blogs);
+  }
+  if (target === "users" || target === "all") {
+    const users = await User.find({});
+    console.log("users:", users);
+  }
+};
+
+const main = async () => {
+  switch (MODE) {
+    case GET_MODE: {
+      const target = process.argv[3];
+      await getInfo(target);
+      break;
+    }
+  }
+  mongoose.connection.close();
+};
+
+main();
